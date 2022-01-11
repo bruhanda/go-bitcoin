01a26f67c52d166858054f8dfa3c15af76675b64
leffw <93302536+leffw@users.noreply.github.com>
2022-01-11 09:24:48 -0300

    Add files via upload

---
+++ bitcoin.go
@@ -0,0 +1,141 @@
+package bitcoin
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/tidwall/gjson"
+)
+
+type Config struct {
+	Host string
+	User string
+	Pass string
+}
+
+type Bitcoin struct {
+	*Config
+}
+
+func Connect(host string, user string, pass string) *Bitcoin {
+	bitcoin := &Bitcoin{
+		Config: &Config{
+			Host: host,
+			User: user,
+			Pass: pass,
+		},
+	}
+
+	if _, err := bitcoin.GetBlockCount(); err != nil {
+		log.Fatal(err)
+	}
+	return bitcoin
+}
+
+func (b Bitcoin) Call(params map[string]interface{}) (gjson.Result, error) {
+	data, err := json.Marshal(params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+
+	req, err := http.NewRequest("POST", b.Host, bytes.NewBuffer(data))
+	if err != nil {
+		return gjson.Result{}, err
+	}
+	client := &http.Client{}
+
+	req.SetBasicAuth(b.User, b.Pass)
+	res, err := client.Do(req)
+	if err != nil {
+		return gjson.Result{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return gjson.Result{}, err
+	}
+
+	rjson := gjson.ParseBytes(body)
+	if rjson.String() == "" {
+		err = fmt.Errorf("Username / password invalid.")
+		return gjson.Result{}, err
+	}
+
+	if rjson.Get("error").String() != "" {
+		err = fmt.Errorf(rjson.Get("error").Get("message").String())
+		return gjson.Result{}, err
+	}
+	return rjson.Get("result"), nil
+}
+
+func (b Bitcoin) GetBlockCount() (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "getblockcount",
+		"params": []interface{}{},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) GetTransaction(txid string) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "gettransaction",
+		"params": []interface{}{txid},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) ListUnspent() (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "listunspent",
+		"params": []interface{}{},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) ImportAddress(address string, label string) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "importaddress",
+		"params": [2]string{address, label},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) GetNewAddress(label string) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "getnewaddress",
+		"params": [1]string{label},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) CreateRawTransaction(inputs []map[string]interface{}, outputs []map[string]interface{}) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "createrawtransaction",
+		"params": []interface{}{inputs, outputs},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) FundRawTransaction(rawtx string, feerate float64) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "fundrawtransaction",
+		"params": []interface{}{rawtx, map[string]interface{}{
+			"feeRate": feerate * 1000,
+		}},
+	}
+	return b.Call(data)
+}
+
+func (b Bitcoin) SignRawTransactionWithWallet(rawtx string) (gjson.Result, error) {
+	data := map[string]interface{}{
+		"method": "signrawtransactionwithwallet",
+		"params": []interface{}{rawtx},
+	}
+	return b.Call(data)
+}
